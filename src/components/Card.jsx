97d740cf7c9d26d9bfbe6545ecc319b51136603e
victoria vassileva <victoria.vassileva@yahoo.fr>
2020-08-18 14:35:03 +0200

    api call

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Card extends React.Component {
+
+    render() {
+
+        const { card } = this.props;
+
+        return (
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{card.name || 'Unknown'}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">{card.gender || 'Unknown'}</h6>
+                    <p class="card-text">{card.culture || 'Unknown'}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Card
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
+import Card from './Card'
 
-const Cards = ({ cards }) => {
-  return (
-    <div>
-      {cards.map((card) => (
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">{card.name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">{card.email}</h6>
-            <p class="card-text">{card.company.catchPhrase}</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  )
-};
+class Cards extends React.Component {
+
+  state = {
+    cards: JSON.parse('[{ "url": "https://anapioficeandfire.com/api/characters/1", "name": "", "gender": "Female", "culture": "Braavosi", "born": "", "died": "", "titles": [""], "aliases": ["The Daughter of the Dusk"], "father": "", "mother": "", "spouse": "", "allegiances": [], "books": ["https://anapioficeandfire.com/api/books/5"], "povBooks": [], "tvSeries": [""], "playedBy": [""] }, { "url": "https://anapioficeandfire.com/api/characters/2", "name": "Walder", "gender": "Male", "culture": "", "born": "", "died": "", "titles": [""], "aliases": ["Hodor"], "father": "", "mother": "", "spouse": "", "allegiances": ["https://anapioficeandfire.com/api/houses/362"], "books": ["https://anapioficeandfire.com/api/books/1", "https://anapioficeandfire.com/api/books/2", "https://anapioficeandfire.com/api/books/3", "https://anapioficeandfire.com/api/books/5", "https://anapioficeandfire.com/api/books/8"], "povBooks": [], "tvSeries": ["Season 1", "Season 2", "Season 3", "Season 4", "Season 6"], "playedBy": ["Kristian Nairn"] }, { "url": "https://anapioficeandfire.com/api/characters/3", "name": "", "gender": "Male", "culture": "", "born": "", "died": "", "titles": [""], "aliases": ["Lamprey"], "father": "", "mother": "", "spouse": "", "allegiances": ["https://anapioficeandfire.com/api/houses/15"], "books": ["https://anapioficeandfire.com/api/books/3"], "povBooks": [], "tvSeries": [""], "playedBy": [""] }, { "url": "https://anapioficeandfire.com/api/characters/4", "name": "", "gender": "Female", "culture": "Braavosi", "born": "", "died": "", "titles": [""], "aliases": ["The Merling Queen"], "father": "", "mother": "", "spouse": "", "allegiances": [], "books": ["https://anapioficeandfire.com/api/books/5", "https://anapioficeandfire.com/api/books/8"], "povBooks": [], "tvSeries": [""], "playedBy": [""] }, { "url": "https://anapioficeandfire.com/api/characters/5", "name": "", "gender": "Male", "culture": "", "born": "", "died": "", "titles": [""], "aliases": ["Old Crackbones"], "father": "", "mother": "", "spouse": "", "allegiances": [], "books": ["https://anapioficeandfire.com/api/books/5"], "povBooks": [], "tvSeries": [""], "playedBy": [""] }, { "url": "https://anapioficeandfire.com/api/characters/6", "name": "", "gender": "Female", "culture": "Braavosi", "born": "", "died": "", "titles": [""], "aliases": ["The Poetess"], "father": "", "mother": "", "spouse": "", "allegiances": [], "books": ["https://anapioficeandfire.com/api/books/5"], "povBooks": [], "tvSeries": [""], "playedBy": [""] }, { "url": "https://anapioficeandfire.com/api/characters/7", "name": "", "gender": "Female", "culture": "", "born": "", "died": "", "titles": [""], "aliases": ["Porridge"], "father": "", "mother": "", "spouse": "", "allegiances": ["https://anapioficeandfire.com/api/houses/15"], "books": ["https://anapioficeandfire.com/api/books/3"], "povBooks": [], "tvSeries": [""], "playedBy": [""] }, { "url": "https://anapioficeandfire.com/api/characters/8", "name": "", "gender": "Male", "culture": "", "born": "", "died": "", "titles": [""], "aliases": ["Quickfinger"], "father": "", "mother": "", "spouse": "", "allegiances": ["https://anapioficeandfire.com/api/houses/23"], "books": ["https://anapioficeandfire.com/api/books/6"], "povBooks": [], "tvSeries": [""], "playedBy": [""] }, { "url": "https://anapioficeandfire.com/api/characters/9", "name": "", "gender": "Female", "culture": "", "born": "", "died": "", "titles": [""], "aliases": ["the Sailor\'s Wife"], "father": "", "mother": "", "spouse": "", "allegiances": [], "books": ["https://anapioficeandfire.com/api/books/5"], "povBooks": [], "tvSeries": [""], "playedBy": [""] }, { "url": "https://anapioficeandfire.com/api/characters/10", "name": "", "gender": "Female", "culture": "Braavosi", "born": "", "died": "", "titles": [""], "aliases": ["The Veiled Lady"], "father": "", "mother": "", "spouse": "", "allegiances": [], "books": ["https://anapioficeandfire.com/api/books/5"], "povBooks": [], "tvSeries": [""], "playedBy": [""] }]')
+  }
+  render() {
+
+    const { cards } = this.state;
+
+    return (
+      <div>
+        {cards.map((card) => (<Card card={card} />))}
+      </div>
+    );
+  }
+}
 
 export default Cards
